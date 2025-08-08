d504feae26382a44b2fe7e1873990ff7903ba347
alex <alexandre.ag.67@gmail.com>
2025-08-08 14:37:48 +0200

    chore: pin Node version, add ESLint baseline and Vitest config

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: { react: { version: 'detect' } },
+};
